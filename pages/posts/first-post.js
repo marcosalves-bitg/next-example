2cba86527750fa7362b5b4d881242ca14e93d456
MARCOS RODRIGO JUNG ALVES <digosw@gmail.com>
2021-03-08 15:27:10 -0300

    commit example full

---
+++ pages/posts/first-post.js
@@ -0,0 +1,12 @@
+import Link from 'next/link'
+import Head from 'next/head'
+import Layout from '../../components/layout'
+
+
+export default function FirstPast() {
+  return (
+    <Layout>
+      <h1>Post de Teste</h1>
+    </Layout>
+  )
+}
---
+++ pages/test.js
@@ -0,0 +1,12 @@
+import Link from 'next/link'
+import Head from 'next/head'
+import Layout from '../components/layout'
+
+
+export default function Test() {
+  return (
+    <Layout>
+      <h1>Página de Teste</h1>
+    </Layout>
+  )
+}
